d2870c9f7ada5c3ac4e70ea3ba474d7987d0c496
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-03-28 10:57:08 +0530

    Exercise 3 completed

---
+++ Exercise-3/index.js
@@ -0,0 +1,30 @@
+const { randomSplice } = require("random-splice");
+const fs = require("fs").promises;
+const http = require("http");
+
+async function createAndReadRandomColors(res) {
+  //reading from color palette json
+  let colorPalette;
+  let colorPaletteLength;
+  colorPalette = JSON.parse(await fs.readFile("./color_palette.json", "utf-8"));
+  colorPaletteLength = colorPalette.length;
+
+  //writing to random color palette json
+  let randomColorPalette = [];
+  for (let itemCount = 0; itemCount < 5; itemCount++) {
+    randomColorPalette.push(randomSplice(colorPalette));
+  }
+  await fs.writeFile("./random_color_palette.json", JSON.stringify(randomColorPalette));
+
+  //reading from random color palette json
+  colorPalette = await fs.readFile("./random_color_palette.json", "utf-8");
+  console.log(colorPalette);
+  res.write(colorPalette);
+}
+
+http
+  .createServer(async (req, res) => {
+    await createAndReadRandomColors(res);
+    res.end();
+  })
+  .listen(4000);
