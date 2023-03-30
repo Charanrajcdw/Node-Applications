27a2fb1d77d3d86d202eb215ca1cae45234043fa
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-03-30 19:04:40 +0530

    try catch implemented

--- Exercise-3/index.js
+++ Exercise-3/index.js
@@ -1,21 +1,26 @@
 const http = require("http");
-const {randomSplice}=require("random-splice");
-const { readColorFile, writeColorFile } = require("./file_operations");
+const { randomSplice } = require("random-splice");
+const { readColorFile, writeColorFile } = require("./file.utils.js");
 
 async function createAndReadRandomColors(res) {
-  //reading from color palette
-  let colorPalette = await readColorFile("./color_palette.json");
+  try {
+    //reading from color palette
+    let colorPalette = await readColorFile("./color_palette.json");
 
-  //writing to random color palette
-  let randomColorPalette = [];
-  for (let itemCount = 0; itemCount < 5; itemCount++) {
-    randomColorPalette.push(randomSplice(colorPalette));
+    //writing to random color palette
+    let randomColorPalette = [];
+    for (let itemCount = 0; itemCount < 5; itemCount++) {
+      randomColorPalette.push(randomSplice(colorPalette));
+    }
+    await writeColorFile("./random_color_palette.json", JSON.stringify(randomColorPalette));
+
+    //reading from random color palette
+    colorPalette = await readColorFile("./random_color_palette.json");
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(colorPalette));
+  } catch (err) {
+    res.write(String(err));
   }
-  await writeColorFile("./random_color_palette.json", JSON.stringify(randomColorPalette));
-
-  //reading from random color palette
-  colorPalette = await readColorFile("./random_color_palette.json");
-  res.write(JSON.stringify(colorPalette));
   res.end();
 }
 
