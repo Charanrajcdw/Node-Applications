eddb2810e8f5e2b17eee834e7ffe02f71d6becdc
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-04-04 18:13:29 +0530

    added logger

--- Exercise-5/controllers/read.controller.js
+++ Exercise-5/controllers/read.controller.js
@@ -1,13 +1,20 @@
 const READ_SERVICE = require("../services/read.service");
+const LOGGER = require("../utils/logger.utils");
 
 const readBuddy = async (req, res) => {
   const BUDDY_ID = req.params.id;
   const RESULT = await READ_SERVICE.readBuddyService(BUDDY_ID);
+  if (RESULT.status == "failure") {
+    LOGGER.error(`IP:${req.ip}, URL:${req.originalUrl}, STATUS:${RESULT.code}, MESSAGE:${RESULT.data}`);
+  }
   res.status(RESULT.code).send(RESULT.data);
 };
 
 const readAllBuddies = async (req, res) => {
   const RESULT = await READ_SERVICE.readAllBuddiesService();
+  if (RESULT.status == "failure") {
+    LOGGER.error(`IP:${req.ip}, URL:${req.originalUrl}, STATUS:${RESULT.code}, MESSAGE:${RESULT.data}`);
+  }
   res.status(RESULT.code).send(RESULT.data);
 };
 
