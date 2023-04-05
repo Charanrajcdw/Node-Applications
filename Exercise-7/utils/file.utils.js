baac04a2fbf1c3a0c2ca5ba8504e8a80f08db3ee
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-04-05 15:58:15 +0530

    Folder structure added

---
+++ Exercise-7/utils/file.utils.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = "user-data.json";
+
+const readFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
+  });
+};
+
+const writeFile = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve("Data written successfully!");
+      }
+    });
+  });
+};
+
+module.exports = { readFile, writeFile };
